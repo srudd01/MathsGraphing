f3f8ae6707b4e5cc88cdeb3a39c72e4f55b4a004
srudd01 <110849156+srudd01@users.noreply.github.com>
2022-10-19 01:14:37 +0100

    Add files via upload
    Dyadic transformation

---
+++ Dyadic.R
@@ -0,0 +1,29 @@
+#The parameter 'n' controls the number of times the dyadic transformation
+#is applied. 'ndef' is the default value of 'n'.
+#We assume 0 <= x <= 1
+ndef <- 41
+dyadic <- function(x, n=ndef){
+  if(n < 1){
+    return(x)
+  }
+  
+  if(x < 1/2){
+    return(dyadic(2*x, n-1))
+  }
+  else{
+    return(dyadic(2*x-1,n-1))
+  }
+}
+
+pdf(file= "dyadic.pdf")
+
+#We only require 60 iterations because when ndef = 60, then the graph is now
+#the straight line at y=0. At this point the transformation will always assign
+#y=0 to 2*y = 0, meaning no further changes will occur.
+for(i in 1:60){
+  ndef <- i
+  x <- seq(0,1, length.out = 10000)
+  y <- lapply(x, dyadic)
+  str <- paste("Dyadic Transformation Iterated", toString(i), "times")
+  plot(x,y, main=str)
+}
